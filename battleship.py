406a72089217ec8b084122f9fcf085ca8c208189
RyanWest09 <grizzwest@hotmail.ca>
2023-02-06 20:11:19 -0800

    Can now add boats to the board with no overlap

--- battleship.py
+++ battleship.py
@@ -28,10 +28,12 @@ class Board:
       grid.append(temp_row)
     self.grid = grid
 
+#prints the grid
   def print_grid(self):
     for rows in self.grid:
       print(rows)  
         
+#changes a selected symbol
   def change_symbol(self,coord, hit):
     x,y = self.coord_mapping[coord]
     if hit:
@@ -39,43 +41,66 @@ class Board:
     else:
       self.grid[x][y] = 'M'
 
-  def add_boat(self,boat_coords):
-    for coords in boat_coords:
-       self.change_symbol(coords,True)
+#adds a boat to the grid
+  def add_boat(self,list_coords):
+    for coord in list_coords:
+      x,y = self.coord_mapping[coord]
+      self.grid[x][y] = 'B'
+    self.print_grid()
 
 
 
 
-class Boats:
-  def __init__(self):
-    self.all_boats = []
 
-  def create_patrol(self):
-    coords = input("Please input 2 Coordinates in the form 'A1,A2,A3,etc' : ")
-    coord_list = coords.split(',')
-    self.all_boats.append(coord_list)
-    print(coord_list)
-    return coord_list
+class Boat:
+  #dictionary containing all coords of all boats
+  all_coords = {}
+  def __init__(self,lives,name):
+    self.name = name
+    self.lives = lives
+    string_coords = input("Please Input {} co-ordinates in the following fashion A1,A2,A3 or B1,C1,D1: ".format(lives))
+    list_coords = string_coords.split(',')
+    invalid = True
+    if self.all_coords == {}:
+      self.all_coords[name] = list_coords
+      invalid = False
+    else:
+      while(invalid):
+        invalid, list_coords = self.check_valid(list_coords)
+        continue
+    self.all_coords[name] = list_coords
+    self.own_coords = list_coords
 
-  def create_carrier(self):
-    coords = input("Please input 5 Coordinates in the form 'A1,A2,A3,etc' : ")
-    coord_list = coords.split(',')
-    self.all_boats.append(coord_list)
-    print(coord_list)
-    return coord_list
+#determines if any boat coordinates will overlap
+  def check_valid(self,list_coords):
+    for coords in list_coords:
+      for valid_coords in self.all_coords.values():
+        if coords in valid_coords or len(list_coords) != self.lives:
+          print("invalid coordinates try again")
+          string_coords = input("Please Input {} co-ordinates in the following fashion A1,A2,A3 or B1,C1,D1: ".format(self.lives))
+          list_coords = string_coords.split(',')
+          return True, list_coords
+        else:
+          return False, list_coords
+          
+          
+      
+      
+     
+  
+        
 
 
 
 
 board = Board(10,10)
 # board.print_grid()
-player1_boats = Boats()
-board.add_boat(player1_boats.create_patrol())
-board.add_boat(player1_boats.create_carrier())
+patrol1 = Boat(2,"patrol1")
+board.add_boat(patrol1.own_coords)
+carrier1 = Boat(5,"Carrier")
+board.add_boat(carrier1.own_coords)
 
 
+print(carrier1.all_coords)
 
-board.change_symbol('F0',False)
-board.change_symbol('F1',True)
-board.print_grid()
 
